f63d62f26571feea5bec17545f546bc10569e144
blueshadow0324 <blueshadow0324@gmail.com>
2026-09-01 20:42:16 +0200

    Remove attachment handling in email function
    Removed attachment handling from email sending function.

--- main.py
+++ main.py
@@ -122,13 +122,7 @@ def send():
     msg["To"] = email
     msg["Subject"] = str(date)
     msg.attach(MIMEText(body, "test - debug"))
-
-    if attachment_path:
-        with open(attachment_path, "rb") as f:
-            part = MIMEApplication(f.read(), Name=os.path.basename(attachment_path))
-        part["Content-Disposition"] = f'attachment; filename="{os.path.basename(attachment_path)}"'
-        msg.attach(part)
-
+    
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
         server.login(email, password)
         server.send_message(msg)

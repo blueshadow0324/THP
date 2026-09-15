691bf3ccd554511886ff73f282545d2d004d01da
blueshadow0324 <blueshadow0324@gmail.com>
2026-09-15 20:28:00 +0200

    Encode email body to UTF-8 before sending

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ def send():
     msg["From"] = sender_email
     msg["To"] = email
     msg["Subject"] = str(date)
+    body = body.encode("utf-8", "replace").decode("utf-8")
     msg.attach(MIMEText(body, "plain", "utf-8"))
     
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
